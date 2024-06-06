653870ee6ec38d9f3ad3a5db4fc24d3926383abe
miludCifp <10395@cifpceuta.es>
2024-06-07 00:46:53 +0200

    Arreglando los archivos estaticos

--- api_incidencias/target/classes/static/js/registro_user.js
+++ api_incidencias/target/classes/static/js/registro_user.js
@@ -205,7 +205,7 @@ async function registrarCliente() {
 
 
     if (validarCampos(tipoDocumento, documento,genero, email, passwd, confirmPasswd, tlfn, ciudad, calle, provincia, codigoPostal, pais)) {
-        const url = 'http://localhost:8080/auth/registrar-cliente';
+        const url = 'http://185.166.39.117:8080/auth/registrar-cliente';
 
         const data = {
             tipoDocumento: tipoDocumento,
